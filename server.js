120f4fc747a18b0f21eaeb7a87c416fa282680ff
Aimee Lewis <aimee.lewis@ufl.edu>
2019-08-31 21:17:29 -0400

    Modified server file

--- server.js
+++ server.js
@@ -1,51 +1,67 @@
-var http = require('http'), 
-    fs = require('fs'), 
+var http = require('http'),
+    fs = require('fs'),
     url = require('url'),
     port = 8080;
 
 /* Global variables */
 var listingData, server;
 
+
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
 
   /*
-    Your request handler should send listingData in the JSON format as a response if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
+  Your request handler should send listingData in the JSON format as a response if a GET request
+  is sent to the '/listings' path. Otherwise, it should send a 404 error.
 
-    HINT: Explore the request object and its properties 
-    HINT: Explore the response object and its properties
-    https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-    
-    HINT: Explore how callback's work 
-    http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
-    
-    HINT: Explore the list of MIME Types
-    https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
-   */
+  HINT: Explore the request object and its properties
+  HINT: Explore the response object and its properties
+  https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
+  http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
+
+  HINT: Explore how callback's work
+  http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
+
+  HINT: Explore the list of MIME Types
+  https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
+  */
+
+  //if a GET request is sent to the '/listings' path
+  if (request.method == 'GET' && parsedUrl.pathname == '/listings') {
+    //send listingData in the JSON format as a response
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write(listingData);
+    response.end();
+
+  }
+  else {
+    response.writeHead(404, {'Content-Type': 'text/plain'});
+    response.end('Bad gateway error');
+  }
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
   /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
+  This callback function should save the data in the listingData variable,
+  then start the server.
 
-    HINT: Check out this resource on fs.readFile
-    //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
+  HINT: Check out this resource on fs.readFile
+  https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
 
-    HINT: Read up on JSON parsing Node.js
-   */
+  HINT: Read up on JSON parsing Node.js
+  https://flaviocopes.com/nodejs-parse-json/
+  */
 
-    //Check for errors
-  
+  //Check for errors - ??
+  if (err) throw err;
 
-   //Save the sate in the listingData variable already defined
-  
+  //Save the sate in the listingData variable already defined
+  listingData = data;
 
   //Creates the server
-  
-  //Start the server
+  http.createServer(requestHandler).listen(8080);
 
+  //Start the server
+  console.log('Server started');
 
 });
